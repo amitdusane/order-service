d8184ddaf31f8f13a64e4b87ae104ab650b39c25
amit-dusane <amit.dusane@turtlemint.com>
2025-04-25 14:31:59 +0530

    add log

--- src/main/java/com/api/order/order_service/GreetingController.java
+++ src/main/java/com/api/order/order_service/GreetingController.java
@@ -1,13 +1,16 @@
 package com.api.order.order_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class GreetingController {
 
     @GetMapping("/api/hello")
     public String sayHello() {
+        log.info("Inside sayhello");
         return "Hello from Order Service!";
     }
 }
