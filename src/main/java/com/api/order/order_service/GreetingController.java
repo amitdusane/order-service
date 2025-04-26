9a6ca45ee027b02e9f9541c5e7f1aba0b3e2475c
amit-dusane <amit.dusane@turtlemint.com>
2025-04-26 11:51:31 +0530

    add property

--- src/main/java/com/api/order/order_service/GreetingController.java
+++ src/main/java/com/api/order/order_service/GreetingController.java
@@ -1,6 +1,7 @@
 package com.api.order.order_service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class GreetingController {
 
+    @Value("${hello.by}")
+    private String helloBy;
+
     @GetMapping("/api/hello")
     public String sayHello() {
         log.info("Inside sayhello");
-        return "Hello from Order Service!";
+        return "Hello from Order Service By - " + helloBy;
     }
 }
